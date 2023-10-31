e8a4881ece2838ba94f659c5ecc0ff3b85766261
YourmixJNR <radmakson@gmail.com>
2023-10-31 21:41:09 +0100

    init

---
+++ src/Context/Components/Switch.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactSwitch from 'react-switch'
+
+const Switch = () => {
+  return (
+    <div>
+      <ReactSwitch 
+      
+      />
+    </div>
+  )
+}
+
+export default Switch
---
+++ src/Context/Pages/Homepage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Switch from '../Components/Switch'
+
+const Homepage = () => {
+  return (
+    <div>
+      <Switch />
+    </div>
+  )
+}
+
+export default Homepage
