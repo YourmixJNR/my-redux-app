7f51bad73ea85496d542a4daa89783d3765406fa
YourmixJNR <radmakson@gmail.com>
2023-11-04 02:11:11 +0100

    more logic

--- src/PropsDrilling/ParentComponent.jsx
+++ src/PropsDrilling/ParentComponent.jsx
@@ -1,17 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ChildComponent from './ChildComponent';
 
 const ParentComponent = () => {
-    const userData = {
+    // State to store the input value
+    const [nameInput, setNameInput] = useState("");
+    const [userData, setUserData] = useState({
         name: "John Doe",
         age: 30,
         address: '123 Main Street',
-    }
-  return (
-    <div>
-      <ChildComponent userData={userData} />
-    </div>
-  )
-}
+    });
 
-export default ParentComponent
+    const handleChange = (e) => {
+        // Update the nameInput state with the typed value
+        setNameInput(e.target.value);
+    };
+
+    const handleSubmit = (e) => {
+        // Prevents the default form submission behavior
+        e.preventDefault();
+        // Update the name in userData with the value stored in nameInput
+        setUserData((prevData) => ({...prevData, name: nameInput}));
+    };
+    
+    return (
+        <div>
+            <input 
+                type='text'
+                value={nameInput}
+                onChange={handleChange}
+            />
+            <button onClick={handleSubmit}>Update</button>
+            <ChildComponent userData={userData} />
+        </div>
+    );
+};
+
+export default ParentComponent;
