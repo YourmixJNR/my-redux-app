bed2df567f63decfc0a323e93a973a384ef0ce2d
YourmixJNR <radmakson@gmail.com>
2023-11-04 03:29:31 +0100

    Logic to Context API

--- src/ContextAPI/ParentComponentContext.jsx
+++ src/ContextAPI/ParentComponentContext.jsx
@@ -1,17 +1,42 @@
 import React from "react";
 import UserContext from "./UserContext/UserContext";
+import { useState } from "react";
 import ChildComponentContext from "./ChildComponentContext";
 
 const ParentComponentContext = () => {
-  const userData = {
+  const [userData, setUserData] = useState({
     name: "John Doe",
     age: 30,
     address: "123 Main Street",
+  });
+
+  const [nameInput, setNameInput] = useState();
+
+  const handleChange = (e) => {
+    // Update the nameInput state with the typed value
+    setNameInput(e.target.value);
   };
+
+  const handleSubmit = (e) => {
+    // Prevents the default form submission behavior
+    e.preventDefault();
+    // Update the name in userData with the value stored in nameInput
+    setUserData((prevData) => ({ ...prevData, name: nameInput }));
+  };
+
   return (
     <div>
       <UserContext.Provider value={userData}>
+
+        <input
+            type="text"
+            value={nameInput}
+            onChange={handleChange} />
+
+        <button onClick={handleSubmit}>Update</button>
+        
         <ChildComponentContext />
+
       </UserContext.Provider>
     </div>
   );
--- src/PropsDrilling/ParentComponent.jsx
+++ src/PropsDrilling/ParentComponent.jsx
@@ -1,38 +1,41 @@
-import React, { useState } from 'react';
-import ChildComponent from './ChildComponent';
+import React, { useState } from "react";
+import ChildComponent from "./ChildComponent";
 
 const ParentComponent = () => {
-    // State to store the input value
-    const [nameInput, setNameInput] = useState("");
-    const [userData, setUserData] = useState({
-        name: "John Doe",
-        age: 30,
-        address: '123 Main Street',
-    });
+  // State to store the input value
+  const [nameInput, setNameInput] = useState("");
+  const [userData, setUserData] = useState({
+    name: "John Doe",
+    age: 30,
+    address: "123 Main Street",
+  });
 
-    const handleChange = (e) => {
-        // Update the nameInput state with the typed value
-        setNameInput(e.target.value);
-    };
+  const handleChange = (e) => {
+    // Update the nameInput state with the typed value
+    setNameInput(e.target.value);
+  };
 
-    const handleSubmit = (e) => {
-        // Prevents the default form submission behavior
-        e.preventDefault();
-        // Update the name in userData with the value stored in nameInput
-        setUserData((prevData) => ({...prevData, name: nameInput}));
-    };
-    
-    return (
-        <div>
-            <input 
-                type='text'
-                value={nameInput}
-                onChange={handleChange}
-            />
-            <button onClick={handleSubmit}>Update</button>
-            <ChildComponent userData={userData} />
-        </div>
-    );
+  const handleSubmit = (e) => {
+    // Prevents the default form submission behavior
+    e.preventDefault();
+    // Update the name in userData with the value stored in nameInput
+    setUserData((prevData) => ({ ...prevData, name: nameInput }));
+  };
+
+  return (
+    <div>
+        
+      <input
+        type="text"
+        value={nameInput}
+        onChange={handleChange} />
+
+      <button onClick={handleSubmit}>Update</button>
+
+      <ChildComponent userData={userData} />
+
+    </div>
+  );
 };
 
 export default ParentComponent;
